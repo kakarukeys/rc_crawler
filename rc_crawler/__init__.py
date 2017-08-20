ec06f1066211897e978d5947bb12d6ab7c1812f4
Wong Jiang Fung <kakarukeys@gmail.com>
2017-08-20 21:09:44 +0800

    added boilerplate files

---
+++ rc_crawler/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""for crawling and scraping product information off major ecommerce platforms"""
+
+from rc_crawler import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ rc_crawler/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'rc_crawler'
+project = "Reconnify Crawler"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'for crawling and scraping product information off major ecommerce platforms'
+authors = ['Wong Jiang Fung']
+authors_string = ', '.join(authors)
+emails = ['kakarukeys@gmail.com']
+license = 'GPL v3'
+copyright = '2017 ' + authors_string
+url = 'https://www.junglescout.com'
