ab13971352f91903289ff55f67a13fb663b32d70
Wong Jiang Fung <kakarukeys@gmail.com>
2017-10-14 22:14:41 +0800

    fetch now supports query params and different file types

--- rc_crawler/crawler/fetch.py
+++ rc_crawler/crawler/fetch.py
@@ -24,24 +24,25 @@ class FetchOutcome(Enum):
     MAYBE_PROXY_FAILURE = "maybe_proxy_failure"
 
 
-async def fetch(session: aiohttp.ClientSession, url: str, extra_headers: dict={}, proxy: str=None) -> None:
-    """ fetch html content from <url>
-        returns {"outcome": ..., (optional) "html": ..., (optional) "reason": "reason for failure"}
+async def fetch(session, url, extra_headers={}, proxy=None, params=None, filetype="text"):
+    """ fetch content from <url> with query <params>
+        filetype: text/json/binary
+        returns {"outcome": ..., (optional) "content": ..., (optional) "reason": "reason for failure"}
     """
     logger = logging.getLogger("rc_crawler.fetch")
 
-    logger.debug("sending request to {0} with extra headers {1}, proxy: {2}".format(
-        url, extra_headers, proxy))
+    logger.debug("sending request to {0} with extra headers {1}, proxy: {2}, params: {3}".format(
+        url, extra_headers, proxy, params))
 
     try:
-        async with session.get(url, headers=extra_headers, proxy=proxy) as response:
-            html = await response.text()
+        async with session.get(url, headers=extra_headers, proxy=proxy, params=params) as response:
+            content = await getattr(response, filetype)()
 
             if response.status == 200:
-                result = {"outcome": FetchOutcome.SUCCESS, "html": html}
+                result = {"outcome": FetchOutcome.SUCCESS, "content": content}
             else:
-                logger.error("non-200 response, url: {0}, request headers: {1}, status: {2}, html: {3}, proxy: {4}".format(
-                    url, response.request_info.headers, response.status, html, proxy))
+                logger.error("non-200 response, url: {0}, request headers: {1}, status: {2}, content: {3}".format(
+                    url, response.request_info.headers, response.status, content))
 
                 if response.status in PROXY_ERROR_STATUS_CODES:
                     result = {"outcome": FetchOutcome.PROXY_FAILURE, "reason": response.status}
